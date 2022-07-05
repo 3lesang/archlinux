fb6add78de97cb95d54aa6263a0d17dba8e1c93d
Minh Sang <88425701+nonesang@users.noreply.github.com>
2022-07-05 09:01:19 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo pacman -S ttf-dejavu ttf-freefont ttf-fira-code
+sudo pacman -S ttf-liberation ttf-droid ttf-roboto terminus-font
+sudo pacman -S git xclip scrot feh neovim xterm mpv xdg-user-dirs
+sudo pacman -S pulseaudio pulseaudio-bluetooth alsa-utils alsa-plugins
